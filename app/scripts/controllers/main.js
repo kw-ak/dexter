251b17a61e42961bf2e9d0e54dada3ac1f287529
Mika <munoz.mika@gmail.com>
2017-09-22 17:35:34 +0200

    add best singer per year table

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -21,20 +21,35 @@ angular.module('dexterApp')
 
 
     var counter = {};
+    var best = {}
+
+    //le base url est définir dans le module de config
     Restangular.one("/").get(query).then(function(data){
         $scope.nhits = data.nhits;
         _.each(data.records, function(item) {
+            //si il existe dans le dico on aggrège le nombre, sinon on créer une nouvelle entrée dans le dico
             if (item.fields.annee in counter){
               counter[item.fields.annee] += item.fields.nbre_de_prets;
             } else {
               counter[item.fields.annee] = item.fields.nbre_de_prets;
             }
-          //console.log(item.recordid);
+
+
+            //si il existe ou que le nombre d'emprunts est inférieur on fait rien
+            if (item.fields.annee in best && item.fields.nbre_de_prets < best[item.fields.annee].nb){
+              //pass
+            }else {
+              //sinon on stocke le meilleur artiste
+              best[item.fields.annee] = {'year':item.fields.annee, 'artist': item.fields.auteur, 'nb': item.fields.nbre_de_prets}
+            }
         });
 
+        //on transforme les données pour le chart
         _.each(counter, function(value, key) {
           $scope.labels.push(key);
           $scope.data.push(value);
         });
+        //on copie dans le scope pour y accéder dans le tableau
+        $scope.bestPerYear = best;
     });
   });
