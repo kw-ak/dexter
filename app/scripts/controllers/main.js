9be3c60e87a60eac916dd68d2c6e16e294f6cdb9
Mika <munoz.mika@gmail.com>
2017-09-22 18:06:58 +0200

    impl top 10 artist of alltime

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,18 +20,19 @@ angular.module('dexterApp')
     $scope.data = [];
 
 
-    var counter = {};
-    var best = {}
+    var perYearCounter = {};
+    var best = {};
+    var allTime = {};
 
     //le base url est définir dans le module de config
     Restangular.one("/").get(query).then(function(data){
         $scope.nhits = data.nhits;
         _.each(data.records, function(item) {
             //si il existe dans le dico on aggrège le nombre, sinon on créer une nouvelle entrée dans le dico
-            if (item.fields.annee in counter){
-              counter[item.fields.annee] += item.fields.nbre_de_prets;
+            if (item.fields.annee in perYearCounter){
+              perYearCounter[item.fields.annee] += item.fields.nbre_de_prets;
             } else {
-              counter[item.fields.annee] = item.fields.nbre_de_prets;
+              perYearCounter[item.fields.annee] = item.fields.nbre_de_prets;
             }
 
 
@@ -42,14 +43,33 @@ angular.module('dexterApp')
               //sinon on stocke le meilleur artiste
               best[item.fields.annee] = {'year':item.fields.annee, 'artist': item.fields.auteur, 'nb': item.fields.nbre_de_prets}
             }
-        });
 
+            /* petit test pour verifier que le champs cote est bien unique et correspond a un seul CD
+            if(item.fields.cote === '099.2 OGR'){
+              console.log(item);
+            }
+            */
+
+            if (item.fields.cote in allTime) {
+              allTime[item.fields.cote].nb += item.fields.nbre_de_prets;
+            } else {
+              allTime[item.fields.cote] = {
+                  'item': item.fields,
+                  'nb' : item.fields.nbre_de_prets
+                };
+            }
+
+
+        });
+        //allTime  = _.sortBy(allTime, ['nb'], ['desc']);
+        console.log(allTime);
         //on transforme les données pour le chart
-        _.each(counter, function(value, key) {
+        _.each(perYearCounter, function(value, key) {
           $scope.labels.push(key);
           $scope.data.push(value);
         });
         //on copie dans le scope pour y accéder dans le tableau
         $scope.bestPerYear = best;
+        $scope.allTime = _.sortBy(allTime, ['nb']);
     });
   });
